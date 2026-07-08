5acfc5fbd126c095329dbdc3c1585a89378c1c67
eseninka <eseninka2706@gmail.com>
2026-07-08 12:06:28 +0300

    edit worker for mqtt

--- WEB/backend/worker_mqtt.py
+++ WEB/backend/worker_mqtt.py
@@ -20,35 +20,19 @@ CONNECT = os.getenv("CONNECT")
 MQTT_topic = ""
 
 
-def calib_data(data):
-    clear_data = {
-        "key" : data["uuid"],
-        "uuid" : data["uuid"],
-        "station_id": data["station_id"],
-        "electric_current": data["electric_current"],
-        "flame": True if data["flame"] >= 100 else False,
-        "gas": data["gas"],
-        "ambient_humidity": data["ambient_humidity"],
-        "ambient_temperature": data["ambient_temperature"],
-        "tank_temperature": int(data["tank_temperature"]),
-        "water_level": round((data["water_level"] / 1023) * 100, 2)
-    }
-    return clear_data
-
-
 def on_message(client, userdata, msg):
     print(f"Топик: {msg.topic}, Сообщение: {msg.payload.decode()}, Тип данных {type(msg.payload.decode())}")
     data = json.loads(msg.payload.decode())
-    clear_data = calib_data(data)
+    connection = None
     try:
         connection = psycopg2.connect(host=HOST, user=NAME_USER, password=PASSWORD, database=DATABASE)
         connection.autocommit = True
         with connection.cursor() as cursor:
             cursor.execute(
                 "insert into sensors(uuid, electric_current, flame, gas, ambient_humidity, ambient_temperature, tank_temperature, water_level) values (%s, %s, %s, %s, %s, %s, %s, %s)",
-                (clear_data["uuid"], clear_data["electric_current"], clear_data['flame'],
-                 clear_data["gas"], clear_data["ambient_humidity"], clear_data["ambient_temperature"],
-                 clear_data["tank_temperature"], clear_data["water_level"]))
+                (data["uuid"], data["electric_current"], data['flame'],
+                 data["gas"], data["ambient_humidity"], data["ambient_temperature"],
+                 data["tank_temperature"], data["water_level"]))
     except Exception as e:
         print(f'[info]: Ошибка {e}')
     finally:
