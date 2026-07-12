acbc344c2782fae90c6eff8bcf9ce468630207cc
eseninka <eseninka2706@gmail.com>
2026-07-12 20:56:59 +0300

    edit API_BASE to https://

--- WEB/frontend/src/api/api.js
+++ WEB/frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "api.smartaf.ru"; // Твой бэкенд FastAPI
+const API_BASE = "https://api.smartaf.ru"; // Твой бэкенд FastAPI
 
 export const Api = {
   async _request(method, path, body) {
