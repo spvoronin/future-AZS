80026de27b11051663b9be3681317c1f513c367b
eseninka <eseninka2706@gmail.com>
2026-08-02 21:56:16 +0300

    запросы к БД, свяяанные с показаниями датчиков переписаны на asyncpg, логика инициализации MQTT вынесена

--- WEB/backend/routers/sensors.py
+++ WEB/backend/routers/sensors.py
@@ -1,22 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-import psycopg2
+from fastapi import APIRouter, Depends, HTTPException, status as http_status
 import os
-from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
 from dependencies import verify_admin, get_current_user
+from database import get_db_pool
+import asyncpg
+from mqtt_client import mqtt_client
 
 load_dotenv()
-
-HOST = os.getenv("HOST")
-NAME_USER = os.getenv("NAME_USER")
-PASSWORD = os.getenv("PASSWORD")
-DATABASE = os.getenv("DATABASE")
-CONNECT = os.getenv("CONNECT")
-
-MQTT_LOGIN = os.getenv("MQTT_LOGIN")
-MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
-MQTT_ADDRESS = os.getenv("MQTT_ADDRESS")
-MQTT_PORT = os.getenv("MQTT_PORT")
 
 UUID = os.getenv("UUID")
 
@@ -25,51 +16,17 @@ router_sensor = APIRouter(
     tags=["Всё, что связано с датчиками"]
 )
 
-mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqtt_client.username_pw_set(MQTT_LOGIN, MQTT_PASSWORD)
-mqtt_client.connect(MQTT_ADDRESS, int(MQTT_PORT))
-mqtt_client.loop_start()
-
 @router_sensor.get("")
-async def get_vol_from_sensor(user: dict = Depends(get_current_user)):
-    connection = None
-    try:
-        connection = psycopg2.connect(host=HOST, user=NAME_USER, password=PASSWORD, database=DATABASE)
-        connection.autocommit = True
-        with connection.cursor() as cursor:
-            cursor.execute(
-                'select uuid, electric_current, flame, gas, ambient_humidity, ambient_temperature, tank_temperature, water_level, voltage from sensors where uuid=%s order by id desc limit 1',
-                (UUID,))
-            data_about_sensors = cursor.fetchone()
-            if data_about_sensors is None:
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail="Данные не найдены"
-                )
-            data_res = {
-                "uuid": data_about_sensors[0],
-                "electric_current": data_about_sensors[1],
-                "flame": data_about_sensors[2],
-                "gas": data_about_sensors[3],
-                "ambient_humidity": data_about_sensors[4],
-                "ambient_temperature": data_about_sensors[5],
-                "tank_temperature": data_about_sensors[6],
-                "water_level": data_about_sensors[7],
-                "voltage" : data_about_sensors[8]
-            }
-        return data_res
-    except HTTPException:
-        raise
-    except Exception as e:
-        print(f'info: ошибка {e}')
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Не удалось получить данные с датчиков"
-        )
-    finally:
-        if connection:
-            connection.close()
-            print('info: коннект закрыт')
+async def get_vol_from_sensor(user: dict = Depends(get_current_user), pool: asyncpg.Pool = Depends(get_db_pool)):
+    async with pool.acquire() as connection:
+        query = 'select uuid, electric_current, flame, gas, ambient_humidity, ambient_temperature, tank_temperature, water_level, voltage from sensors where uuid=$1 order by id desc limit 1'
+        data_about_sensors = await connection.fetchrow(query, UUID)
+        if not data_about_sensors:
+            raise HTTPException(
+                status_code=http_status.HTTP_404_NOT_FOUND,
+                detail="Данные не найдены"
+            )
+    return dict(data_about_sensors)
 
 @router_sensor.post("/pumps/{pumps_id}")
 async def vkl_pump(pumps_id : int, admin_user: dict = Depends(verify_admin)):
@@ -79,6 +36,6 @@ async def vkl_pump(pumps_id : int, admin_user: dict = Depends(verify_admin)):
         return {"status": "ok", "message": f"Команда отправлена в {MQTT_topic}"}
     else:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Не удалось отправить команду на ТРК"
         )
